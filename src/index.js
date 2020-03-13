4d964f114a18ee259dff1aa705f5c63a14bba0fe
ILIA <60580096+Raven164@users.noreply.github.com>
2020-03-13 17:23:37 +0400

    feat: add solutions
    feat: add solutions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var chars = str.split(''),
+      stack = [],
+      openBrackets = [],
+      closeBrackets = [],
+      exception = [],
+      exceptionIndex,
+      closeIndex,
+      openIndex;
+
+  for(let i = 0; i < bracketsConfig.length; i++){
+    if( bracketsConfig[i][0] != bracketsConfig[i][1]){
+      openBrackets[i] = bracketsConfig[i][0];
+      closeBrackets[i] = bracketsConfig[i][1]; 
+    } 
+    else{
+      exception.push(bracketsConfig[i][0]);
+    }
+  }
+
+  for (var i = 0; i < chars.length; i++) {
+    openIndex = openBrackets.indexOf(chars[i]);
+    if (openIndex !== -1) {        
+        stack.push(openIndex);
+        continue;
+    }
+    closeIndex = closeBrackets.indexOf(chars[i]);
+    if (closeIndex !== -1) {
+        openIndex = stack.pop();
+        if (closeIndex !== openIndex) {
+            return false;
+        }
+    }
+    exceptionIndex = exception.indexOf(chars[i]);
+    if (exceptionIndex !== -1){
+      let temp = stack[stack.length - 1];
+        if(temp == chars[i]){
+          stack.splice(stack.length - 1, 1);
+        }
+        else{
+          stack.push(chars[i])
+        }
+     }
+ }
+ return (stack.length !== 0) ? false : true;    
 }
